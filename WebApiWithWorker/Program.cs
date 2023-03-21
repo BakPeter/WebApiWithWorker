aa7c488c98c1a4b2f163a12de1fdade56e604e47
Peter Bakshaev <peterb@aeronautics-sys.com>
2023-03-21 14:29:54 +0200

    logger povideds tests

--- WebApiWithWorker/Program.cs
+++ WebApiWithWorker/Program.cs
@@ -1,10 +1,13 @@
+using System.Text.Json;
 using WebApiWithWorker;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.Swagger;
 using WebApiWithWorker.Configurations;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureAppConfiguration((context, configurationBuilder) =>
@@ -14,6 +17,15 @@ builder.WebHost.ConfigureAppConfiguration((context, configurationBuilder) =>
 });
 // Add services to the container.
 var settings = builder.Configuration.GetSection("Settings").Get<Settings>();
+
+//LogSettings(builder.Services, settings);
+
+//void LogSettings(IServiceCollection services, Settings settings)
+//{
+//    using var provider = services.BuildServiceProvider();
+//    var logger = provider.GetRequiredService<ILogger<Program>>();
+//    logger?.LogInformation($"Settings: {JsonSerializer.Serialize(settings)}");
+//}
 
 builder.Services.AddHostedService<Worker>();
 
@@ -26,6 +38,15 @@ builder.Services.AddSwaggerGen();
 builder.WebHost.UseUrls(settings!.ApplicationUrls.ToArray());
 
 var app = builder.Build();
+
+//var logger = app.Services.GetService<ILogger<Program>>();
+//logger?.LogInformation($"Settings: {JsonSerializer.Serialize(settings)}");
+//app.Logger.LogInformation($"Settings: {JsonSerializer.Serialize(settings)}");
+//using (var serviceScope = app.Services.CreateScope())
+//{
+//    var logger = serviceScope.ServiceProvider.GetService<ILogger<Program>>();
+//    logger?.LogWarning($"Settings: {JsonSerializer.Serialize(settings, new JsonSerializerOptions { WriteIndented = true })}");
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
